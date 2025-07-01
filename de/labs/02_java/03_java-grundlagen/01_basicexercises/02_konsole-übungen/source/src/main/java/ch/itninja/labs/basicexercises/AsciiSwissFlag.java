bff562853d9e4bf6bf3dc422f376b378f8f7d748
andiaeg <andiaeg@users.noreply.github.com>
2025-07-01 13:23:52 +0000

    deploy: c61e34499d65c1882edfd1aecaf79330a1f52a50

---
+++ de/labs/02_java/03_java-grundlagen/01_basicexercises/02_konsole-übungen/source/src/main/java/ch/itninja/labs/basicexercises/AsciiSwissFlag.java
@@ -0,0 +1,21 @@
+package ch.itninja.labs.basicexercises;
+
+public class AsciiSwissFlag {
+
+    private AsciiSwissFlag() {
+        // Prevent instantiation
+    }
+
+    /**
+     * Zeichne eine Schweizer Fahne. Die Fahne muss einen Rahmen haben. Du darfst dazu
+     * folgende Zeichen verwenden:
+     *
+     * * Im Rahmen: '|', '-', '+'', Leerzeichen (' ') 
+     * * Innerhalb: '|', '-', '+', '*', '=', '@'', Leerzeichen (' ')
+     */
+    public static void printSwissFlag(){
+
+        // IT-Ninja: Füge hier Deinen Code ein...
+
+    }
+}
---
+++ de/labs/02_java/03_java-grundlagen/01_basicexercises/03_bedingte-ausführung/source/src/main/java/ch/itninja/labs/basicexercises/HelloName.java
@@ -0,0 +1,22 @@
+package ch.itninja.labs.basicexercises;
+
+
+/**
+ * Utility class providing methods for basic Hello Name output.
+ */
+public class HelloName {
+
+    private HelloName() {
+        // Prevent instantiation
+    }
+
+    /**
+     * Erstelle eine Variable, welche deinen Namen beinhaltet.
+     * Gib Hello [name] auf der Konsole aus.
+     */
+    public static void printHelloName(){
+
+        // IT-Ninja: Füge hier Deinen Code ein...
+
+    }
+}
