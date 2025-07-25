ff3f44f30a507e206125dd9289d47d7a9963bc74
andiaeg <andiaeg@users.noreply.github.com>
2025-07-25 20:21:36 +0000

    deploy: 8e718fe7749d9a2bd8df219180d5eeefcf732c31

---
+++ de/labs/02_java/03_java-grundlagen/06_control-structures-loops/source/src/main/java/ch/itninja/labs/basicexercises/NumberHelper.java
@@ -0,0 +1,50 @@
+package ch.itninja.labs.basicexercises;
+
+/**
+  * Utility class providing helper methods for month operations.
+*/
+public class NumberHelper {
+
+    private NumberHelper() {
+        // Prevent instantiation
+    }
+
+    /**
+     * Passe die folgende Methode an. Zähle alle Zahlen von 1 bis 100 zusammen, welche durch 8 teilbar sind und gib das
+     * Resultat auf der Konsole aus:
+     *
+     * - Die Summe aller durch 8 teilbaren Zahlen von 1 bis 100 beträgt 624.
+     */
+    public static void sumDivisibleByEightFixedRange() {
+
+        // IT-Ninja: Füge hier Deinen Code ein...
+    }
+
+    /**
+     * Passe die folgende Methode an. Zähle alle Zahlen von min bis max zusammen, welche durch divisor teilbar sind und gib das
+     * Resultat auf der Konsole aus:
+     *
+     * - Die Summe aller durch [divisor] teilbaren Zahlen von [min] bis [max] beträgt [result].
+     *
+     * Abgrenzung: Wenn max kleiner als min ist oder wenn divisor kleiner gleich 0 ist, soll folgender Text auf der
+     * Konsole ausgegeben werden: Berechnung mit diesen Werten nicht möglich
+     */
+    public static void sumDivisibleBy(int min, int max, int divisor) {
+
+        // IT-Ninja: Füge hier Deinen Code ein...
+    }
+
+    /**
+     * Passe die folgende Methode an. Zähle alle Zahlen von min bis max zusammen, welche ein gemeinsames Vielfaches von den
+     * beiden Zahlen number1 und number2sind und gib das Resultat auf der Konsole aus:
+     *
+     * - Summe der Zahlen von [min] bis [max], die ein gemeinsames Vielfache von [number1] und [number2] sind: [result].
+     *
+     * Abgrenzung: Wenn max kleiner als min ist oder wenn eine der Zahlen number1 oder 'number2' kleiner gleich 0 ist,
+     * soll folgender Text auf der Konsole ausgegeben werden: Berechnung mit diesen Werten nicht möglich
+     */
+    public static void sumCommonMultiples(int min, int max, int number1, int number2) {
+
+        // IT-Ninja: Füge hier Deinen Code ein...
+    }
+}
