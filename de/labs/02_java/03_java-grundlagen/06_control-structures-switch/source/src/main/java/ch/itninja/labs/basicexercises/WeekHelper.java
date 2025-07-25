4a117a5dbf8e6ff504b9ff032c87ca1e052f90bd
andiaeg <andiaeg@users.noreply.github.com>
2025-07-25 16:09:44 +0000

    deploy: 76896d0dfd5f187c7cbd12baad4713e301027043

---
+++ de/labs/02_java/03_java-grundlagen/06_control-structures-switch/source/src/main/java/ch/itninja/labs/basicexercises/WeekHelper.java
@@ -0,0 +1,27 @@
+package ch.itninja.labs.basicexercises;
+
+/**
+ * Utility class providing helper methods for week operations.
+ */
+public class WeekHelper {
+
+    private WeekHelper() {
+        // Prevent instantiation
+    }
+
+    /**
+     * Passe die folgende Methode an. Gib abhängig von der Variable weekdayName auf der Konsole aus, um welchen Wochentag es sich
+     * handelt:
+     *
+     * - Der [weekdayName] ist der [weekday]. Tag in der Woche.
+     * - [weekdayName] entspricht keinem bekannten Wochentag.
+     *
+     * Wobei in der Ausgabe [weekdayName] durch den Namen des Wochentags ersetzt werden soll und [weekday] durch den Tag
+     * der Woche, welche dem Wochentag entspricht. Der Montag soll dabei der erste Tag in der Woche sein.
+     */
+    public static void printWeekdayNumber(String weekdayName) {
+
+        // IT-Ninja: Füge hier Deinen Code ein...
+
+    }
+}
